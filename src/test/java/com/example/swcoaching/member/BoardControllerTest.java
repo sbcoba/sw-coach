668803c75163d389335ebb06bca09a2225945c01
leesuhong <leesuhong@woowahan.com>
2022-07-06 23:38:11 +0900

    Github Action 에서 테스트 진행 추가
    테스트 구조 변경 등

--- src/test/java/com/example/swcoaching/member/BoardControllerTest.java
+++ src/test/java/com/example/swcoaching/member/BoardControllerTest.java
@@ -1,29 +1,51 @@
 package com.example.swcoaching.member;
 
+import com.example.swcoaching.board.Board;
+import com.example.swcoaching.board.BoardService;
+import com.example.swcoaching.controller.BoardController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(BoardController.class)
 class BoardControllerTest {
-  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+
+  @MockBean
+  private BoardService boardService;
   @Autowired
   private MockMvc mockMvc;
 
-  @DisplayName("게시판을 조회 한다")
+  @DisplayName("게시판 정보를 조회 테스트 한다")
   @Test
-  void getMember() throws Exception {
-    mockMvc.perform(get("/board/{boardId}", 1))
-            .andDo(print())
-            .andExpect(jsonPath("$.id").value(is(1)));
+  void testBoard() throws Exception {
+    // given
+    long id = 1L;
+    String title = "test";
+    String remark = "test1";
+    when(boardService.findById(id)).thenReturn(new Board(id, title, remark, emptyList()));
+
+    // when
+    ResultActions resultActions = mockMvc.perform(get("/board/{boardId}", id)).andDo(print());
+
+    // then
+    resultActions
+            .andExpect(jsonPath("$.id").value(is((int) id)))
+            .andExpect(jsonPath("$.title").value(is(title)))
+            .andExpect(jsonPath("$.remark").value(is(remark)));
   }
 }
--- src/test/java/com/example/swcoaching/member/MemberControllerTest.java
+++ src/test/java/com/example/swcoaching/member/MemberControllerTest.java
@@ -1,29 +1,52 @@
 package com.example.swcoaching.member;
 
+import com.example.swcoaching.controller.BoardController;
+import com.example.swcoaching.controller.MemberController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MemberController.class)
 class MemberControllerTest {
-  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   private MockMvc mockMvc;
 
-  @DisplayName("회원을 조회 한다")
+  @MockBean
+  private MemberService memberService;
+
+  @DisplayName("회원을 조회 테스트를 한다")
   @Test
-  void getMember() throws Exception {
-    mockMvc.perform(get("/member?username=skku_student1"))
-            .andDo(print())
-            .andExpect(jsonPath("$.username").value(is("skku_student1")));
+  void testMember() throws Exception {
+
+    // given
+    long id = 1L;
+    String username = "test_user";
+    String password = "password";
+    String name = "테스트유저";
+    String remark = "비고";
+    when(memberService.findByUsername(username)).thenReturn(new Member(id, username, password, name, remark));
+
+    // when
+    ResultActions resultActions = mockMvc.perform(get("/member?username=" + username)).andDo(print());
+
+    // then
+    resultActions
+            .andExpect(jsonPath("$.id").value(is((int) id)))
+            .andExpect(jsonPath("$.username").value(is(username)))
+            .andExpect(jsonPath("$.password").value(is(password)))
+            .andExpect(jsonPath("$.name").value(is(name)))
+            .andExpect(jsonPath("$.remark").value(is(remark)));
   }
 }
